2bc365eae53114f5f57ca6bdb64710dd6582575d
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-09-10 14:24:37 -0400

    commit

--- ArrayChallengeDeuce2/ViewController.swift
+++ ArrayChallengeDeuce2/ViewController.swift
@@ -19,8 +19,89 @@ class ViewController: UIViewController {
 
     
     
-    // Create your methods here
-
-
+    func addNameToDeliLine(name:String) -> String {
+        
+        var upNext: String = ""
+        
+        deliLine.append(name)
+        
+        for (index, name) in deliLine.enumerate() {
+            
+            let numInLine = index + 1
+            
+            switch name {
+                
+                case "Billy Crystal":
+                    deliLine.insert(name, atIndex: 0)
+                    upNext = "Welcome Billy! You can sit wherever you like."
+                
+                case "Meg Ryan":
+                    deliLine.insert(name, atIndex: 0)
+                    upNext = "Welcome Meg! You can sit wherever you like."
+                
+                case deliLine[0]:
+                    upNext = "Welcome \(name), you're first in line!"
+                
+                default:
+                    upNext = "Welcome \(name), you're number \(numInLine) in line."
+            }
+            
+            
+        }
+        
+        return upNext
+        
+    }
+    
+    
+    func nowServing() -> String {
+        
+        var nowServingString = ""
+        
+        if deliLine.count == 0 {
+            
+            nowServingString = "There is no one to be served."
+            
+        } else {
+            
+            for (index, name) in deliLine.enumerate() {
+                
+                nowServingString = "Now serving \(name)!"
+                deliLine.removeAtIndex(index)
+                
+            }
+        }
+        
+        return nowServingString
+    }
+    
+    
+    
+    func deliLineDescription() -> String {
+        
+        var deliLineDescriptionString = ""
+        
+        if deliLine.count == 0 {
+            
+            deliLineDescriptionString = "The line is currently empty."
+            
+        } else {
+            
+            deliLineDescriptionString = "The line is:\n"
+            
+            for (index,name) in deliLine.enumerate() {
+                
+                let num = index + 1
+                
+                deliLineDescriptionString.appendContentsOf("\(num). \(name)")
+            }
+            
+        }
+        
+        
+        return deliLineDescriptionString
+    }
+    
+    
 }
 
